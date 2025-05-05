c1ee6658f79b86fc18424ae87c7509c2c8442f77
Caiquesantanadias <81467566+Caiquesantanadias@users.noreply.github.com>
2025-05-05 12:47:43 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <App />
   </BrowserRouter>
 )
